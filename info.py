35d563c2d5b09f977990fbcb557323252f3aa62d
Priyansh-commits-3005 <117034550+Priyansh-commits-3005@users.noreply.github.com>
2023-04-23 04:18:54 +0530

    added web3 implementation and changed info.py to accomodate accordingly

--- info.py
+++ info.py
@@ -1,3 +1,12 @@
 host='localhost'
 user='root'
 password='mysql'
+
+# transaction
+# user 
+account_1 = input(" enter acc number")
+private_key = input("input private key")
+
+#admin
+account_2 = ""# gonna ask the admin to get his acc number here 
+
